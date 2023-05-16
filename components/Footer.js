d4d740c3e8c39008e60164c170afd216a2315e7a
nkapzamalov <115000079+nkapzamalov@users.noreply.github.com>
2023-05-16 15:40:29 +0300

    footer p

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-mypurple">
-      <div className="mx-auto max-w-7xl px-6 py-12 mt-72">
+      <div className="mx-auto max-w-7xl px-6 py-12 mt-24">
         <p className="text-center text-xs leading-5 text-white">
           &copy; 2022 RiseBlog, Inc. All rights reserved.
         </p>
